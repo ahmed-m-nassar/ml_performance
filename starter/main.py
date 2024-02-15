d92520990caba3155c695e90ad09ccf566de29d0
Ahmed Mohamed Nassar <ahmed.nassar.cmp@gmail.com>
2024-02-15 13:53:08 +0300

    modified main

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,7 @@
 # Put the code for your API here.
+def test_answer(cmdopt = "type1"):
+    if cmdopt == "type1":
+        print("first")
+    elif cmdopt == "type2":
+        print("second")
+    assert 0  # to see what was printed
