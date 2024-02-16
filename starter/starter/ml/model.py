1d2ee73c6d4c0240be505fc52435f17da5641d41
Ahmed Mohamed Nassar <ahmed.nassar.cmp@gmail.com>
2024-02-16 14:51:07 +0300

    training model

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV    
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -17,8 +18,24 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
+    # Define the hyperparameters to tune
+    param_grid = {
+        'n_estimators': [200],  # Number of trees in the forest
+        'max_depth': [10, 20],       # Maximum depth of the trees
+        'min_samples_split': [2],   # Minimum number of samples required to split a node
+    }
+    
+    # Create a RandomForestClassifier
+    rf = RandomForestClassifier()
 
-    pass
+    # Grid search with cross-validation
+    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=5, n_jobs=-1)
+    grid_search.fit(X_train, y_train)
+
+    # Get the best model
+    best_model = grid_search.best_estimator_
+    
+    return best_model
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +74,5 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    
+    return model.predict(X)
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,10 +1,17 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+from ml.data import process_data
+from ml.model import train_model,compute_model_metrics , inference
+import pandas as pd
+import joblib
+import os
 
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
+data = pd.read_csv("nd0821-c3-starter-code//starter//data//census.csv")
+data.rename(columns=lambda x: x.replace(' ', ''), inplace=True)
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -22,7 +29,27 @@ cat_features = [
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
+# Train and save a model.
+model = train_model(X_train,y_train)
+model_filename = 'nd0821-c3-starter-code//starter//model//trained_model.pkl'
+encoder_filename = 'nd0821-c3-starter-code//starter//model//encoder.pkl'
+
+# Save the trained model to disk
+joblib.dump(model, model_filename)
+joblib.dump(encoder, encoder_filename)
 
 # Proces the test data with the process_data function.
+# model = joblib.load("nd0821-c3-starter-code//starter//model//trained_model.pkl")
+# print("model loaded")
 
-# Train and save a model.
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False , encoder = encoder ,lb = lb
+)
+print("processed data")
+
+predictions = inference(model , X_test)
+print("inference done")
+print(predictions)
+
+print(compute_model_metrics(y_test,predictions))
+
